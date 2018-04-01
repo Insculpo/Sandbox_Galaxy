0e734eec00de93b4332eba78f855cad51cb2a2c0
Trenten <tkaufeld@ucsc.edu>
2018-04-01 09:51:28 -0700

    Protype tagging
    It'll be good

---
+++ Associated_Coding_Projects/Galaxy_Placer/js/World_Builder.js
@@ -0,0 +1,10 @@
+function Build_World(name) {
+  var tagger = name.split(" ");
+  Console.log("BUILD");
+
+  for(var k = 0; k < tagger.length; k++)
+  {
+    Console.log(tagger[i]);
+  }
+//  return World_Object;
+}
